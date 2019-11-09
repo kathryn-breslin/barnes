33a4acfa9e1c3482feecff83085275b56672c635
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-11-09 08:46:39 -0500

    add four new heros to state

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,47 +1,88 @@
 import React, { Component } from "react";
 import json from "../../utils/json";
 
+const newHeros = [
+{
+    name: "Melinda Gates", 
+    age: "55", 
+    secretIdentity: "Bill & Melinda Gates Foundation", 
+    powers: [
+        "Kindness",
+        "Empathy", 
+        "Influence" 
+    ]
+}, 
+{
+    name: "Warren Buffet", 
+    age: "89", 
+    secretIdentity: "Berkshire Hathaway", 
+    powers: [
+        "Patience",
+        "Dedication", 
+        "Honesty" 
+    ]
+}, 
+{
+    name: "Elon Musk", 
+    age: "48", 
+    secretIdentity: "SpaceX", 
+    powers: [
+        "Productivity",
+        "Creativity", 
+        "Risk-tolerant" 
+    ]
+}, 
+{
+    name: "Mark Zuckerberg", 
+    age: "35", 
+    secretIdentity: "Facebook", 
+    powers: [
+        "Innovation",
+        "Assertiveness", 
+        "Problem-solving" 
+    ]
+}];
+
 class HeroService extends Component {
+  state = {
+    heros: [],
+    squadName: "",
+    homeTown: "",
+    secretBase: "",
+    formed: ""
+  };
+  componentDidMount() {
+    this.squadInfo();
+  }
 
-    state = {
-        heros: [], 
-        squadName: "", 
-        homeTown: "", 
-        secretBase: "", 
-        formed: ""
-    }
+  squadInfo() {
+    json.heros().then(res => {
+      this.setState({
+        heros: [...res.data.members, newHeros],
+        squadName: res.data.squadName,
+        homeTown: res.data.homeTown,
+        secretBase: res.data.secretBase,
+        formed: res.data.formed
+      });
+    });
+  }
 
-    componentDidMount() {
-        this.squadInfo();
-    }
+  render() {
+    const { heros, squadName, homeTown, secretBase, formed } = this.state;
+    console.log("State: " + squadName);
+    console.log("Members: " + JSON.stringify(heros))
+    // console.log("Members: " + heros.name)
+    return (
+      <div>
+        <h1>Hero Section!</h1>
 
-    squadInfo () {
-        json.heros().then(res => {
-            this.setState({ 
-                squadName: res.data.squadName, 
-                homeTown: res.data.homeTown, 
-                secretBase: res.data.secretBase, 
-                formed: res.data.formed
-            })
-        })
-    }
-    
-    render () {
-
-        const { squadName, homeTown, secretBase, formed } = this.state; 
-        console.log("State: " + squadName)
-        return (
-            <div>
-                <h1>Hero Section!</h1>
-                
-
-                <h1>{"Squad Name: " + squadName}</h1>
-                <h2>{"Hometown:" + homeTown}</h2>
-                <p>{"Secret Base: " + secretBase}</p>
-                <p>{"Formed: " + formed}</p>
-            </div>
-        )
-    }
+        <h1>{"Squad Name: " + squadName}</h1>
+        <h2>{"Hometown:" + homeTown}</h2>
+        <p>{"Secret Base: " + secretBase}</p>
+        <p>{"Formed: " + formed}</p>
+      </div>
+    );
+  }
 }
 
 export default HeroService;
