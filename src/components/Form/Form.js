3f2485e347c97594863069b5ddfef7c8df3f71e5
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-11-10 12:40:41 -0500

    form collapse on valid submission

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import HeroService from "../Hero/Hero";
-// import { Button, Collapse } from "react-bootstrap";
+import { Collapse } from "react-bootstrap";
 import "./Form.css";
+
 
 class Form extends Component {
   //Set the state for user input
@@ -16,8 +17,10 @@ class Form extends Component {
     emailError: "",
     zipError: "",
     validated: false,
-    formOpen: false
+    formOpen: false,
+    toggle: true
   };
+
 
   //function to handle the event change when user types
   handleInputChange = event => {
@@ -33,12 +36,7 @@ class Form extends Component {
     event.preventDefault();
     console.log("Submit has been clicked!");
 
-    const {
-      name,
-      phoneNumber,
-      email,
-      zip,
-    } = this.state;
+    const { name, phoneNumber, email, zip } = this.state;
 
     this.setState({
       name: name,
@@ -52,132 +50,149 @@ class Form extends Component {
     console.log("Email: " + email);
     console.log("Zip: " + zip);
 
-      if (!name.length) {
-        console.log("Please add your name");
-        this.setState({ nameError: "Please add your name. Ex: 'Jane Doe'" });
-      } else {
-        this.validated();
-      }
-      if (phoneNumber.length < 7 && isNaN) {
-        console.log("Please enter a valid number.");
-        this.setState({
-          phoneError: "Please enter a valid number. Ex: 6101231234"
-        });
-      } else {
-        this.validated();
-      }
-      if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
-        console.log("Working email");
-        this.validated();
-      } else {
-        console.log("Please add a valid email.");
-        this.setState({
-          emailError: "Please add a valid email. Ex: email@email.com"
-        });
-      }
-      if (zip.length < 5 && isNaN) {
-        console.log("Please add a valid zip.");
-        this.setState({ zipError: "Please add valid zipcode. Ex: 19234" });
-      } else {
-        this.validated();
-      }
+    if (!name.length) {
+      console.log("Please add your name");
+      this.setState({ nameError: "Please add your name. Ex: 'Jane Doe'" });
+    } else {
+      // this.setState({ nameError: "" });
+
+      this.validated();
     }
+    if (phoneNumber.length < 7 && isNaN) {
+      console.log("Please enter a valid number.");
+      this.setState({
+        phoneError: "Please enter a valid number. Ex: 6101231234"
+      });
+    } else {
+      // this.setState({ phoneError: "" });
+      this.validated();
+    }
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
+      console.log("Working email");
+      // this.setState({ emailError: "" });
+      this.validated();
+    } else {
+      console.log("Please add a valid email.");
+      this.setState({
+        emailError: "Please add a valid email. Ex: email@email.com"
+      });
+    }
+    if (zip.length < 5 && isNaN) {
+      console.log("Please add a valid zip.");
+      this.setState({ zipError: "Please add valid zipcode. Ex: 19234" });
+    } else {
+      // this.setState({ zipError: "" });
+      this.validated();
+    }
+  };
+  // };
 
   //Function to hide form on successful submittion of the form
   validated = () => {
-        this.setState({ validated: true })
-        if (this.state.validated) {
-          this.setState({ formOpen: true})
-          console.log("Form open state: " + this.state.formOpen)
-          this.setState({ nameError: "", phoneError: "", emailError: "", zipError: "" });
-      }
-      else {
-        console.log("Form has not been validated")
-      }
-  
+    console.log("Form has been validated! Time to hide the form.");
+    // console.log("Name Error: " + nameError + " | Phone Error: " + phoneError)
+    this.setState({ validated: true });
+    if (this.state.validated) {
+      this.setState({ formOpen: true });
+      this.collapeForm(); 
+      console.log("Form open state: " + this.state.formOpen);
+      this.setState({ nameError: "", phoneError: "", emailError: "", zip: "" });
+    } else {
+      console.log("Form has not been validated");
+    }
   };
 
+  collapeForm () {
+    this.setState({ toggle: !this.state.toggle })
+  }
   render() {
+
+    // const open = (this.state.toggle) ? "show" : "";
+
     return (
       <div>
-        {/* <Collapse> */}
         <div className="container" id="form">
           <div className="card">
             <div className="card-header">Personal Information</div>
             <div className="card-body">
-              <form>
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    className={`form-control ${
-                      this.state.nameError ? "is-invalid" : ""
-                    }`}
-                    id="name"
-                    name="name"
-                    placeholder="name"
-                    onChange={this.handleInputChange}
-                  />
-                  <div className="invalid">{this.state.nameError}</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="phone">Phone Number</label>
-                  <input
-                    type="text"
-                    className={`form-control ${
-                      this.state.phoneError ? "is-invalid" : ""
-                    }`}
-                    id="phoneNumber"
-                    name="phoneNumber"
-                    placeholder="(610) 898-3456"
-                    onChange={this.handleInputChange}
-                  />
-                  <div className="invalid">{this.state.phoneError}</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email">Email address</label>
-                  <input
-                    type="email"
-                    className={`form-control ${
-                      this.state.emailError ? "is-invalid" : ""
-                    }`}
-                    id="email"
-                    name="email"
-                    aria-describedby="email"
-                    placeholder="Enter email"
-                    onChange={this.handleInputChange}
-                  />
-                  <div className="invalid">{this.state.emailError}</div>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="zip">Zip Code</label>
-                  <input
-                    type="text"
-                    className={`form-control ${
-                      this.state.zipError ? "is-invalid" : ""
-                    }`}
-                    id="zip"
-                    name="zip"
-                    placeholder="19382"
-                    onChange={this.handleInputChange}
-                  />
-                  <div className="invalid">{this.state.zipError}</div>
-                </div>
-                <button
-                  id="submit"
-                  type="submit"
-                  className="btn btn-outline-dark my-2 my-sm-0 btn-block"
-                  onClick={this.handleFormSubmit} 
-                >
-                  Continue
-                </button>
-              </form>
+              {/* <Collapse> */}
+              {this.state.toggle ? (
+                  <form>
+                  <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                      type="text"
+                      className={`form-control ${
+                        this.state.nameError ? "is-invalid" : ""
+                      }`}
+                      id="name"
+                      name="name"
+                      placeholder="name"
+                      onChange={this.handleInputChange}
+                    />
+                    <div className="invalid">{this.state.nameError}</div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="phone">Phone Number</label>
+                    <input
+                      type="text"
+                      className={`form-control ${
+                        this.state.phoneError ? "is-invalid" : ""
+                      }`}
+                      id="phoneNumber"
+                      name="phoneNumber"
+                      placeholder="(610) 898-3456"
+                      onChange={this.handleInputChange}
+                    />
+                    <div className="invalid">{this.state.phoneError}</div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input
+                      type="email"
+                      className={`form-control ${
+                        this.state.emailError ? "is-invalid" : ""
+                      }`}
+                      id="email"
+                      name="email"
+                      aria-describedby="email"
+                      placeholder="Enter email"
+                      onChange={this.handleInputChange}
+                    />
+                    <div className="invalid">{this.state.emailError}</div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="zip">Zip Code</label>
+                    <input
+                      type="text"
+                      className={`form-control ${
+                        this.state.zipError ? "is-invalid" : ""
+                      }`}
+                      id="zip"
+                      name="zip"
+                      placeholder="19382"
+                      onChange={this.handleInputChange}
+                    />
+                    <div className="invalid">{this.state.zipError}</div>
+                  </div>
+                  <button
+                    id="submit"
+                    type="submit"
+                    className="btn btn-outline-dark my-2 my-sm-0 btn-block"
+                    onClick={this.handleFormSubmit}
+                  >
+                    Continue
+                  </button>
+                </form>
+              ) : ""}
+            
               {/* </Collapse> */}
-
-              {this.state.formOpen ? <HeroService /> : null}
             </div>
           </div>
         </div>
+        <div className="container" id="hero">
+            {this.state.formOpen ? <HeroService /> : null}
+          </div>
       </div>
     );
   }
